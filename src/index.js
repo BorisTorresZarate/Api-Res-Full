4bfcdfbd5b45bc7c0be45e481897fe88c6aa7832
BorisTorres <bogui231297@gmail.com>
2024-05-03 13:44:30 -0500

    segundo commit

--- src/index.js
+++ src/index.js
@@ -5,23 +5,9 @@ import rolesRouters from './routes/roles.routes.js'
 import publicacionesRouters from './routes/publicaciones.routes.js'
 import comentarioRouters from './routes/comentario.routes.js'
 import categoriaRouters from './routes/categorias.routes.js'
-
+import {swaggerDocs} from './swaggerUI.js'
 const app = express()
 app.use(express.json())
-
-/* //cors
-app.use((req,res,next)=>{
-    const {origin} = req.headers
-
-    if(origin === undefined) return next()
-
-    const permitidos = ['http://127.0.0.1:5500']
-
-    if(permitidos.includes(origin)){
-        res.setHeader('Access-Control-Allow-Origin',origin)
-        next();
-    }
-}) */
 
 //rutas
 app.use(usuariosRouters)
@@ -30,5 +16,7 @@ app.use(publicacionesRouters)
 app.use(comentarioRouters)
 app.use(categoriaRouters)
 
+//swagger Documentacion
+swaggerDocs(app)
 //crear servidor
 app.listen(PORT,() => console.log(`servidor corriendo en http://localhost:${PORT}`))
---
+++ src/swaggerUI.js
@@ -0,0 +1,6 @@
+import express from 'express';
+const app = express();
+import { serve, setup } from 'swagger-ui-express';
+import swaggerDocument from './swagger-output.json' assert {type: "json"};
+
+export const swaggerDocs= (app) => app.use('/api-docs', serve, setup(swaggerDocument))
